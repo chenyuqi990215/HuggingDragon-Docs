2dd576471f5999aad2c90f5d3938620723b80e9a
chenyuqi990215 <10175101214@stu.ecnu.edu.cn>
2023-07-16 10:34:09 +0800

    add bigrocket

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,9 +63,5 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_logo = "_static/logo.svg"
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
 
 source_suffix = ['.rst', '.md']
